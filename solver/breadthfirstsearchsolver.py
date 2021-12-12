6d0b8dd589c274470c51c6cf53a9d2d3dad34b1f
Leonard Keith Franco <leonard.k.franco@gmail.com>
2021-12-12 12:14:04 -0800

    Finish Breadth First Solver

---
+++ solver/breadthfirstsearchsolver.py
@@ -0,0 +1,12 @@
+from .solver import Solver
+from maze.node import Node
+
+class BreadthFirstSearchSolver(Solver):
+    def __init__(self, maze) -> None:
+        super().__init__(maze)
+
+    def addToOpenList(self, node: Node):
+        self.openList.append(node)
+
+    def takeFromOpenList(self):
+        return self.openList.pop(0)
