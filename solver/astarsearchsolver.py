ce51046b8f23b794a6f957bb99299c258147eefc
Leonard Keith Franco <leonard.k.franco@gmail.com>
2021-12-12 23:37:32 -0800

    Finish A* Solver

---
+++ solver/astarsearchsolver.py
@@ -0,0 +1,16 @@
+from .solver import Solver
+from maze.node import Node
+import heapq
+import random
+
+class AStarSearchSolver(Solver):
+    def __init__(self, maze) -> None:
+        super().__init__(maze)
+
+    def addToOpenList(self, node: Node):
+        totalCost = self.calculateCurrentTotalCost(node)
+
+        heapq.heappush(self.openList, (totalCost + node.h, random.random(), node))
+
+    def takeFromOpenList(self):
+        return heapq.heappop(self.openList)[~0]
