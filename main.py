0477d7f57072abe3c17091be7fc1b991155a201a
Leonard Keith Franco <leonard.k.franco@gmail.com>
2021-12-16 22:34:35 -0800

    Format change

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from driver import Driver
 import time
 
 def main():
-    print("= Program - Start =")
+    print("=== Program - Start ===")
     timer = time.perf_counter()
 
     driver = Driver(Maze(rowLength=2000, columnLength=2000))
     driver.start()
 
     timer = time.perf_counter() - timer
-    print(f"= Program - Finish (Execution time: {timer:.4f} sec) =")
+    print(f"=== Program - Finish (Execution time: {timer:.4f} sec) ===")
 
 if __name__ == "__main__": main()
