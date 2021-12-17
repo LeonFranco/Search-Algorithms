896f0c1e48362b79fadb28ec727b69b3ae3ebd0f
Leonard Keith Franco <leonard.k.franco@gmail.com>
2021-12-16 22:33:59 -0800

    Time overall program

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from maze.maze import Maze
 from driver import Driver
+import time
 
 def main():
-    driver = Driver(Maze(rowLength=1000, columnLength=1000))
+    print("= Program - Start =")
+    timer = time.perf_counter()
+
+    driver = Driver(Maze(rowLength=2000, columnLength=2000))
     driver.start()
+
+    timer = time.perf_counter() - timer
+    print(f"= Program - Finish (Execution time: {timer:.4f} sec) =")
 
 if __name__ == "__main__": main()
